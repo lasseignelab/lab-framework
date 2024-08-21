ea4b7a6bd8905cc0a3d4fd0ad3fa5f91fdbc318d
Tonie Crumley <tonie.crumley@gmail.com>
2024-08-21 11:42:45 -0500

    Add help command
    The help command is implemented by convention.  All commands are
    stored in the commands folder.  For a command "foo", the script
    implementing the command will be named foo.sh.  It must contain
    functions named foo, foo_description, and foo_help.  Function
    foo_description will be used to list all commands with a brief
    description.  Function foo_help will be used for full help text
    for the command.  Function foo will implement the command.

--- README.md
+++ README.md
@@ -21,6 +21,49 @@ The `lab` CLI provides commands to help with reproducible research.
 lab <command> params...
 ```
 
+## help
+Shows help for the lab command line tool.
+
+Definition:
+```
+lab help [COMMAND]
+```
+Example:
+```
+$ lab help
+
+  Commands:
+
+  help  Shows help for the lab command line tool.
+  md5   Calculates a combined MD5 checksum for one or more files.
+
+$ lab help md5
+
+  Calculates a combined MD5 checksum for one or more files.
+
+  The "md5" command produces a combined MD5 checksum for all the files
+  specified.  It will show a list of all files included to ensure that the
+  result is as expected.
+
+  Usage:
+    lab md5 FILE...
+
+    FILE... can be one or more file and/or directory specifications.
+
+  Example:
+    $ lab md5 *
+
+    Files included:
+    43bd364a97a38fb1da7c57e6381886c1  lab-cli/LICENSE
+    b794df25f796ac80680c0e4d27308bce  lab-cli/commands/md5.sh
+    0d9281c3586c420130bcb5d25c8a151a  lab-cli/lab
+    5e79c988140af1b7bd5735b0bf96306b  lab-cli/README.md
+    783a44ffae97afbce3f1649c5ff517a5  lab-cli/install.sh
+
+    Combined MD5 checksum:
+    a225199964b84bdeef33bafe3df7c10b
+```
+
 ## md5
 The `lab md5` command will produce an md5sum for the file or files specified.
 This makes it easy to determine whether files are identical.
---
+++ commands/help.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+help_description() {
+  cat <<EOF
+  Shows help for the lab command line tool.
+EOF
+}
+
+help_help() {
+  help_description
+  echo
+
+  cat <<EOF
+  The "help" command will display help for all the commands available for the
+  lab command.
+
+  Usage:
+    lab help [COMMAND]
+
+    COMMAND - optional parameter of command to show help for. If not command
+      is provided then a list of all commands with a brief description will
+      be shown.
+
+  Example:
+    $ lab help
+
+    Commands:
+
+    help  Shows help for the lab command line tool.
+    md5   Calculates a combined MD5 checksum for one or more files.
+EOF
+}
+
+help() {
+  echo
+
+  # Check if a parameter was provided
+  if [ "$#" -eq 0 ]; then
+    cat <<EOF
+  Usage: lab COMMAND ...
+
+  Commands:
+    The following subcommands are available.
+
+  COMMAND
+EOF
+
+    # Directory containing the scripts
+    COMMANDS_DIR="$SCRIPT_DIR"/commands
+
+    {
+      # Loop through each script file in the directory
+      for script in "$COMMANDS_DIR"/*.sh; do
+        # Get the base name of the script (e.g., md5 for md5.sh)
+        script_name=$(basename "$script" .sh)
+
+        # Construct the function name
+        description_function="${script_name}_description"
+
+        # Check if the function exists
+        if declare -f "$description_function" > /dev/null; then
+          printf '    '$script_name':'
+
+          # Call the function
+          "$description_function"
+        else
+          echo "Function $description_function not found in $script"
+        fi
+      done
+    } | column -t -s ':'
+  else
+    # Retrieve the command name from the first parameter
+    command_name=$1
+    # Construct the function name
+    help_function="${command_name}_help"
+    # Call the function
+    "$help_function"
+  fi
+  echo
+}
