92922a333d7c4b2cdb3e4553afd1a69c974ebd34
Tonie Crumley <tonie.crumley@gmail.com>
2024-08-23 09:49:07 -0500

    Improve documentation

--- commands/new.sh
+++ commands/new.sh
@@ -22,18 +22,19 @@ new_help() {
     PROJECT_NAME   Name of the project which will match the Github repo name.
 
   Example:
-    $ lab new lasseignelab PKD_Expression_CxG
+    $ lab new lasseignelab PKD_Research
 
-    Cloning into 'PKD_Expression_CxG'...
+    Cloning into 'PKD_Research'...
     done.
 
-    Create an empty repository for 'PKD_Expression_CxG' on GitHub by using the
+    Create an empty repository for 'PKD_Research' on GitHub by using the
     following link and settings:
 
       https://github.com/organizations/lasseignelab/repositories/new
 
       * No template
-      * Owner 'lasseignelab'
+      * Owner: lasseignelab
+      * Repository name: PKD_Research
       * Private
       * No READEME file
       * No .gitignore
@@ -41,7 +42,7 @@ new_help() {
 
     Once the Github repository has been created, run the following commands to
     upload the new project to Github:
-      cd PKD_Expression_CxG
+      cd PKD_Research
       git push origin main
 
     Happy researching!!!
@@ -83,7 +84,8 @@ following link and settings:
   https://github.com/organizations/$github_account/repositories/new
 
   * No template
-  * Owner '$github_account'
+  * Owner: $github_account
+  * Repository name: $project_name
   * Private
   * No READEME file
   * No .gitignore
